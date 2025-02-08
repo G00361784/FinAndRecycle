c33b0be69eec83325ad288f86b56d4773c9721fc
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2025-02-08 10:43:28 +0000

    Initial Commit

---
+++ FinAndRecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinAndRecycle
+//
+//  Created by Joseph Mccole on 08/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
