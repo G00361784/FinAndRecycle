ca6226409dcf4a05641b20c64203f0dc24f3cea4
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2025-03-01 15:24:11 +0000

    new functionality now working news now in ui

--- FinAndRecycle/Controllers/ViewController.swift
+++ FinAndRecycle/Controllers/ViewController.swift
@@ -6,80 +6,61 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var collectionView: UICollectionView!
     //var collectionView: UICollectionView!
-    let items = [
-            "Item 1": DetailViewController1(), // Associate item title with a view controller
-            "Item 2": DetailViewController2(),
-            "Item 3": DetailViewController3(),
-            "Item 4": DetailViewController1(), // Example of reusing view controllers
-            "Item 5": DetailViewController2(),
-            "Item 6": DetailViewController3(),
-            
-        ]
-        //let apiKey = "f869c8bcd91543ac9b9689504470c0be"
-    
-    struct NewsResponse: Codable {
-        let articles: [Article]
-    }
+    var articles: [Article] = []  // Store fetched articles
 
-    struct Article: Codable {
-        let title: String?
-        let description: String?
-        let urlToImage: String?
-        // Add other fields as needed
-    }
-
-    func fetchNews(completion: @escaping (Result<[Article], Error>) -> Void) {
-        let apiKey = "f869c8bcd91543ac9b9689504470c0be" 
-        let urlString = "https://newsapi.org/v2/everything?q=oceanenviroment&apiKey=\(apiKey)" // Example endpoint
-
-        guard let url = URL(string: urlString) else {
-            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
-            return
+        struct NewsResponse: Codable {
+            let articles: [Article]
         }
 
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
+        struct Article: Codable {
+            let title: String?
+            let description: String?
+            let urlToImage: String?
+        }
 
-            guard let data = data else {
-                completion(.failure(NSError(domain: "No data received", code: 0, userInfo: nil)))
-                return
-            }
+        func fetchNews() {
+            let apiKey = "f869c8bcd91543ac9b9689504470c0be"
+            let query = "Ocean Environment"
+            let urlString = "https://newsapi.org/v2/everything?q=\(query)&language=en&sortBy=publishedAt&apiKey=\(apiKey)"
 
-            do {
-                let decoder = JSONDecoder()
-                let newsResponse = try decoder.decode(NewsResponse.self, from: data)
-                completion(.success(newsResponse.articles))
-            } catch {
-                completion(.failure(error))
-            }
-        }.resume()
-    }
+            guard let url = URL(string: urlString) else { return }
 
-    // Example usage:
-    
-    
-        
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+
+                guard let data = data else {
+                    print("No data received")
+                    return
+                }
+
+                do {
+                    let decoder = JSONDecoder()
+                    let newsResponse = try decoder.decode(NewsResponse.self, from: data)
+                    DispatchQueue.main.async {
+                        self.articles = newsResponse.articles
+                        self.collectionView.reloadData()
+                    }
+                } catch {
+                    print("Decoding error: \(error)")
+                }
+            }.resume()
+        }
+
         override func viewDidLoad() {
             super.viewDidLoad()
             view.backgroundColor = .systemBackground
-            title = "Home"
-            
-            fetchNews { result in
-                switch result {
-                case .success(let articles):
-                    for article in articles {
-                        print(article.title ?? "No Title")
-                    }
-                case .failure(let error):
-                    print("Error: \(error)")
-                }
-            }
-            
+            title = "News Feed"
+
+            setupCollectionView()
+            fetchNews()
+        }
+
+        func setupCollectionView() {
             let layout = UICollectionViewFlowLayout()
-            layout.itemSize = CGSize(width: 200, height: 100)
+            layout.itemSize = CGSize(width: 300, height: 120)
             layout.minimumInteritemSpacing = 10
             layout.minimumLineSpacing = 10
             layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
@@ -89,10 +70,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             collectionView.register(FeedCell.self, forCellWithReuseIdentifier: "FeedCell")
             collectionView.dataSource = self
             collectionView.delegate = self
-
-            DispatchQueue.main.async {
-                self.scrollView.contentSize = self.collectionView.contentSize
-            }
         }
 
         // MARK: - UICollectionViewDataSource
@@ -102,30 +79,29 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         }
 
         func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return items.count
+            return articles.count
         }
 
         func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCell", for: indexPath) as! FeedCell
-            let itemTitle = Array(items.keys)[indexPath.row] // Get the item title
-            cell.configure(with: itemTitle, target: self, action: #selector(buttonTapped(_:)))
+            let article = articles[indexPath.row]
+            cell.configure(with: article.title ?? "No Title", target: self, action: #selector(articleTapped(_:)))
             cell.button.tag = indexPath.row
             return cell
         }
 
-        // MARK: - Button Action
-        @objc func buttonTapped(_ sender: UIButton) {
+        // MARK: - Handle Article Tap
+        @objc func articleTapped(_ sender: UIButton) {
             let index = sender.tag
-            let itemTitle = Array(items.keys)[index]
-            let destinationVC = items[itemTitle]! // Get the associated view controller
+            let selectedArticle = articles[index]
 
-            print("Button tapped at index: \(index)")
-            print("Selected item: \(itemTitle)")
-
-            navigationController?.pushViewController(destinationVC, animated: true)
+            let detailVC = DetailViewController()
+            detailVC.article = selectedArticle
+            navigationController?.pushViewController(detailVC, animated: true)
         }
     }
 
+    // MARK: - FeedCell (Collection View Cell)
     class FeedCell: UICollectionViewCell {
         let button = UIButton()
 
@@ -147,46 +123,24 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             button.layer.cornerRadius = 8
         }
 
-        func configure(with item: String, target: Any?, action: Selector?) {
-            button.setTitle(item, for: .normal)
+        func configure(with title: String, target: Any?, action: Selector?) {
+            button.setTitle(title, for: .normal)
             button.addTarget(target, action: action!, for: .touchUpInside)
         }
     }
 
+    // MARK: - DetailViewController (For News Details)
+    class DetailViewController: UIViewController {
+        var article: ViewController.Article?
 
-    // Example Detail View Controllers (Create as many as you need)
-
-    class DetailViewController1: UIViewController {
         override func viewDidLoad() {
             super.viewDidLoad()
             view.backgroundColor = .white
-            title = "Details 1"  // Set a specific title
-            let label = UILabel(frame: view.bounds)
-            label.text = "Detail View 1"
-            label.textAlignment = .center
-            view.addSubview(label)
-        }
-    }
+            title = article?.title ?? "News Details"
 
-    class DetailViewController2: UIViewController {
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .white
-            title = "Details 2" // Set a specific title
             let label = UILabel(frame: view.bounds)
-            label.text = "Detail View 2"
-            label.textAlignment = .center
-            view.addSubview(label)
-        }
-    }
-
-    class DetailViewController3: UIViewController {
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            view.backgroundColor = .white
-            title = "Details 3" // Set a specific title
-            let label = UILabel(frame: view.bounds)
-            label.text = "Detail View 3"
+            label.text = article?.description ?? "No Description"
+            label.numberOfLines = 0
             label.textAlignment = .center
             view.addSubview(label)
         }
