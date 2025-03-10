afb5d33404f87a060ffb9b3e5c055fd99591490b
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2025-03-10 16:47:10 +0000

    removed maps picture functionality as firebase does not support it

--- FinAndRecycle/Controllers/MapsViewController.swift
+++ FinAndRecycle/Controllers/MapsViewController.swift
@@ -11,119 +11,68 @@ import FirebaseDatabase
 import FirebaseStorage
 
 
-class MapsViewController: UIViewController, MKMapViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MapsViewController: UIViewController, MKMapViewDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
 
-        
+        @IBOutlet weak var mapView: MKMapView!
+
         var ref: DatabaseReference! // Firebase Database reference
-        var storageRef: StorageReference! // Firebase Storage reference
         var selectedCoordinate: CLLocationCoordinate2D?
-        
+
         override func viewDidLoad() {
             super.viewDidLoad()
             mapView.delegate = self
-            
+
             ref = Database.database().reference() // Initialize Firebase Database
-            storageRef = Storage.storage().reference() // Initialize Firebase Storage
-            
+
             // Show user location
             mapView.showsUserLocation = true
             mapView.userTrackingMode = .follow
-            
+
             // Load existing pins and listen for new ones
             loadPinsFromFirebase()
-            
+
             // Add Long Press Gesture to Add Pins
             let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(_:)))
             mapView.addGestureRecognizer(longPressGesture)
         }
-        
+
         @objc func handleLongPress(_ gesture: UILongPressGestureRecognizer) {
-            
+
             if gesture.state == .began {
                 let location = gesture.location(in: mapView)
                 let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
-                
+
                 selectedCoordinate = coordinate
-                
+
                 // Ask user for pin title
-                let alert = UIAlertController(title: "New Pin", message: "Enter a title and optionally add a photo", preferredStyle: .alert)
+                let alert = UIAlertController(title: "New Pin", message: "Enter a title", preferredStyle: .alert)
                 alert.addTextField()
-                let addPhotoAction = UIAlertAction(title: "Add Photo", style: .default) { _ in
-                    self.presentImagePicker()
-                }
                 let addAction = UIAlertAction(title: "Add Pin", style: .default) { _ in
                     let title = alert.textFields?.first?.text ?? "Untitled Pin"
-                    self.addPin(coordinate: coordinate, title: title, imageUrl: nil)
-                    self.savePinToFirebase(coordinate: coordinate, title: title, imageUrl: nil)
+                    self.addPin(coordinate: coordinate, title: title)
+                    self.savePinToFirebase(coordinate: coordinate, title: title)
                 }
-                alert.addAction(addPhotoAction)
                 alert.addAction(addAction)
                 alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
                 present(alert, animated: true)
             }
         }
-        
-        func presentImagePicker() {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = .photoLibrary
-            present(imagePicker, animated: true)
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[.originalImage] as? UIImage, let coordinate = selectedCoordinate {
-                uploadImage(image, coordinate: coordinate)
-            }
-            dismiss(animated: true)
-        }
-        
-        func uploadImage(_ image: UIImage, coordinate: CLLocationCoordinate2D) {
-            guard let imageData = image.jpegData(compressionQuality: 0.8) else { return }
-            let imageName = UUID().uuidString
-            let imageRef = storageRef.child("pins/\(imageName).jpg")
-            
-            imageRef.putData(imageData, metadata: nil) { metadata, error in
-                if let error = error {
-                    print("Error uploading image: \(error.localizedDescription)")
-                    return
-                }
-                
-                imageRef.downloadURL { url, error in
-                    if let imageUrl = url?.absoluteString {
-                        let alert = UIAlertController(title: "Enter Pin Title", message: "Your image is uploaded!", preferredStyle: .alert)
-                        alert.addTextField()
-                        alert.addAction(UIAlertAction(title: "Add Pin", style: .default) { _ in
-                            let title = alert.textFields?.first?.text ?? "Untitled Pin"
-                            self.addPin(coordinate: coordinate, title: title, imageUrl: imageUrl)
-                            self.savePinToFirebase(coordinate: coordinate, title: title, imageUrl: imageUrl)
-                        })
-                        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-                        self.present(alert, animated: true)
-                    }
-                }
-            }
-        }
-        
-        func addPin(coordinate: CLLocationCoordinate2D, title: String, imageUrl: String?) {
+
+        func addPin(coordinate: CLLocationCoordinate2D, title: String) {
             let annotation = MKPointAnnotation()
             annotation.coordinate = coordinate
             annotation.title = title
             mapView.addAnnotation(annotation)
         }
-        
-        func savePinToFirebase(coordinate: CLLocationCoordinate2D, title: String, imageUrl: String?) {
-            var pinData: [String: Any] = [
+
+        func savePinToFirebase(coordinate: CLLocationCoordinate2D, title: String) {
+            let pinData: [String: Any] = [
                 "latitude": coordinate.latitude,
                 "longitude": coordinate.longitude,
                 "title": title
             ]
-            
-            if let imageUrl = imageUrl {
-                pinData["imageUrl"] = imageUrl
-            }
-            
+
             ref.child("pins").childByAutoId().setValue(pinData) { error, _ in
                 if let error = error {
                     print("Error saving pin: \(error.localizedDescription)")
@@ -132,7 +81,7 @@ class MapsViewController: UIViewController, MKMapViewDelegate, UIImagePickerCont
                 }
             }
         }
-        
+
         func loadPinsFromFirebase() {
             ref.child("pins").observe(.childAdded) { snapshot in
                 guard let data = snapshot.value as? [String: Any] else { return }
@@ -140,42 +89,47 @@ class MapsViewController: UIViewController, MKMapViewDelegate, UIImagePickerCont
                    let lon = data["longitude"] as? CLLocationDegrees,
                    let title = data["title"] as? String {
                     let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
-                    let imageUrl = data["imageUrl"] as? String
-                    self.addPin(coordinate: coordinate, title: title, imageUrl: imageUrl)
+                    self.addPin(coordinate: coordinate, title: title)
                 }
             }
         }
-        
-        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+
+        //MARK: - MKMapViewDelegate methods
+
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            // Don't customize the user location annotation view
+            if annotation is MKUserLocation {
+                return nil
+            }
+
+            let identifier = "CustomPin"
+            var annotationView: MKMarkerAnnotationView
+
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+                annotationView = dequeuedView
+                annotationView.annotation = annotation
+            } else {
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView.canShowCallout = true // Enable callouts
+
+                // Set up a button for the callout's detail disclosure
+                let rightButton = UIButton(type: .detailDisclosure)
+                annotationView.rightCalloutAccessoryView = rightButton
+            }
+            return annotationView
+        }
+
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            // Handle the tap on the callout accessory (detail disclosure button).
             guard let annotation = view.annotation else { return }
-            let title = annotation.title ?? "Unknown"
-            
-            ref.child("pins").queryOrdered(byChild: "title").queryEqual(toValue: title).observeSingleEvent(of: .value) { snapshot in
-                for child in snapshot.children {
-                    if let snap = child as? DataSnapshot, let data = snap.value as? [String: Any], let imageUrl = data["imageUrl"] as? String {
-                        if let imageUrl = data["imageUrl"] as? String {
-                            self.showPinDetails(title: (title ?? title)!, imageUrl: imageUrl)
-                        }                    }
-                }
+
+            if let title = annotation.title {
+                showPinDetails(title: title ?? "No Title")
             }
         }
-        
-        func showPinDetails(title: String, imageUrl: String) {
+
+        func showPinDetails(title: String) {
             let alert = UIAlertController(title: title, message: "", preferredStyle: .alert)
-            let imageView = UIImageView(frame: CGRect(x: 10, y: 50, width: 250, height: 250))
-            imageView.contentMode = .scaleAspectFill
-            
-            if let url = URL(string: imageUrl) {
-                URLSession.shared.dataTask(with: url) { data, _, _ in
-                    if let data = data {
-                        DispatchQueue.main.async {
-                            imageView.image = UIImage(data: data)
-                            alert.view.addSubview(imageView)
-                        }
-                    }
-                }.resume()
-            }
-            
             alert.addAction(UIAlertAction(title: "OK", style: .default))
             present(alert, animated: true)
         }
